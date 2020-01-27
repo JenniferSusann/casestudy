846a8d600b2599bd192b04d2dc680e34c1a9d3a3
ck <fritz.meier@hotmail.ch>
2020-01-27 18:44:23 +0100

    div. Anpassungen/ Registration erweitert/Login pruefen

---
+++ registration_action.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>Formulare</title>
+    </head>
+    <body>
+        <p>Hallo <?php echo $_POST['nachname'] ." ". $_POST['vorname']; ?> </p>
+        <p>E-Mail lautet: <?php echo $_POST['email']; ?> </p>
+        <p>PW ist: <?php echo $_POST['password'] ." Bestätigung: ". $_POST['password_confirm']; ?> </p>
+    </body>
+</html>
