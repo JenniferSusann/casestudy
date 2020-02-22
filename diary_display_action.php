b4e1c138634bd30c3be70317bf8b67828e08ac22
ck <ck@mail.ch>
2020-02-22 19:17:16 +0100

    fast letzte anpassung vor Ubernahme mit neuem

--- diary_display_action.php
+++ diary_display_action.php
@@ -1,6 +1,23 @@
 
 <?php
-    //#zurueckbutton zur Ueversicht
+    //$delete_select_zeile = $_POST['delete_select_zeile'];
+    //var_dump($_POST['delete_zeile']);
+    var_dump($_POST['delete_select_zeile']);
+    foreach ($_POST['delete_select_zeile'] as $value) {
+        echo "Zeile: $value soll gelöscht werden";
+    }
+    /*
+    echo "Var: $delete_zeile";
+    if (isset($_POST['select_zeile'])) {
+        echo "Delet ist gewählt : $delete_zeile";
+        
+    }
+
+    else {
+        echo "Nicht gewählt: ";
+        //var_dump($_POST['delete_zeile']);
+    }
+    */
                 
 ?>
 
