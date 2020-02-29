ffa7b3315cf70ac83d7d8221b857df867cf4eb04
ck <fritz.meier@hotmail.ch>
2020-02-29 22:45:06 +0100

    create

--- create_db.sql
+++ create_db.sql
@@ -1,4 +1,3 @@
-sql
 
 CREATE TABLE users ( 
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -7,53 +6,16 @@ CREATE TABLE users (
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE kategorie (
-	katid INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	id INT NOT NULL,
-	beschreibung VARCHAR(255)
-);
-
 CREATE TABLE eintrag (
-	buchid INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	id INT NOT NULL, 
-	title VARCHAR(255) NOT NULL, -- ck Ist dies ein Input Feld beim Erfassen des Eintrages?
-	-- ck oder ist der title der Eintragstext? JM das söt de titel von gsamte ihtrag sie
-	text VARCHAR(1000) NOT NULL,
-	katid1 INT, 
-	katid2 INT,
-	katid3 INT,
-	datum_eintrag date NOT NULL,
-	bild longblob,
-	created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	user_id INT NOT NULL, 
+	title VARCHAR(255) NOT NULL,
+	textbe VARCHAR(1000) NOT NULL,
+	date_create DATE NOT NULL,
+	create_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	kategorie INT
 );
 
 ALTER TABLE eintrag
-	ADD CONSTRAINT FK_katid1 FOREIGN KEY (katid1)
-		REFERENCES kategorie (katid)
-			ON DELETE RESTRICT
-			ON UPDATE CASCADE;
-
-ALTER TABLE eintrag
-	ADD CONSTRAINT FK_katid2 FOREIGN KEY (katid2)
-		REFERENCES kategorie (katid)
-			ON DELETE RESTRICT
-			ON UPDATE CASCADE;
-
-ALTER TABLE eintrag
-	ADD CONSTRAINT FK_katid3 FOREIGN KEY (katid3)
-		REFERENCES kategorie (katid)
-			ON DELETE RESTRICT
-			ON UPDATE CASCADE;
-
-ALTER TABLE kategorie
-	ADD CONSTRAINT 
-
-CREATE TABLE images(
-	buchid INT(11) AUTO_INCREMENT,
-	katid INT,
-	imgdata longblob, 
-	eintragid INT NOT NULL, 
-	imgtype VARCHAR(20),
-	id INT NOT NULL,
-	PRIMARY KEY (bildid)
-);
+	ADD CONSTRAINT FK_user_id FOREIGN KEY (user_id)
+		REFERENCES users (id);
